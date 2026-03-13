0b369145da71617666a3e684fc18e4f7a94deaa4
YasmimNascimentoH <yasmimnascimentoh@gmail.com>
2026-03-12 21:22:22 -0300

    Iniciando projeto com spring boot

---
+++ src/main/java/com/example/bibliocanto/BibliocantoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliocanto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliocantoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliocantoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bibliocanto/BibliocantoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliocanto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliocantoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
